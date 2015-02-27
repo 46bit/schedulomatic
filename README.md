35bcf5dac1e3666cbff2153bc76c862481cb9d64
Michael Mokrysz <hi@46b.it>
2015-02-27 20:47:29 +0000

    Initial commit. Successfully signing in to eVision but need to detect when the strange loading process completes.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Schedulomatic
+Find gaps in the teaching schedule of a group of University of York students.
+
+## Install
+Install [CasperJS and PhantomJS](http://docs.casperjs.org/en/latest/installation.html).
+
+Run the eVision script as `casperjs --username=mm911 --password=hunter2 bin/evision-navigate.js`.
---
+++ bin/evision-navigate.js
@@ -0,0 +1,35 @@
+var utils = require("utils");
+var casper = require('casper').create({
+  verbose: true,
+  logLevel: 'debug'
+});
+
+if (!casper.cli.has('username') || !casper.cli.has('password')) {
+  casper.echo('Must provide username and password for eVision.', 'ERROR');
+  var calledFile = casper.cli['raw']['args'][0];
+  casper.echo('casperjs --username=mm911 --password=hunter2 ' + calledFile, 'ERROR');
+  casper.exit();
+}
+
+var evision_username = casper.cli.get('username'),
+    evision_password = casper.cli.get('password');
+
+casper.start();
+casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
+
+casper.thenOpen('https://evision.york.ac.uk', function evisionSignIn () {
+  var expectedSignInUrl = (this.getCurrentUrl() == 'https://shib.york.ac.uk/idp/Authn/UserPassword');
+  casper.echo(this.getCurrentUrl(), expectedSignInUrl ? 'INFO' : 'WARNING');
+
+  this.fill('form[name=login_form]', {
+    j_username: evision_username,
+    j_password: evision_password
+  }, true);
+});
+
+casper.then(function () {
+  casper.echo(this.getCurrentUrl(), 'INFO');
+  casper.debugHTML();
+});
+
+casper.run();
